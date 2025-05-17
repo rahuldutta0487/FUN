6f711a2d88df268f9b8341232ac1ed169a67b88b
Rahul Dutta <125856707+rahuldutta0487@users.noreply.github.com>
2025-05-17 12:14:44 +0530

    Time: 105 ms (28.09%), Space: 45 MB (55.06%) - LeetHub

--- 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.java
+++ 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.java
@@ -1,46 +1,48 @@
 class Solution {
     public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
-        int n = groups.length;
-        int[] dp = new int[n];
-        int[] prev = new int[n];
+        int n = words.length;
+        int[] dp = new int[n]; 
+        int[] prev = new int[n]; 
         Arrays.fill(dp, 1);
         Arrays.fill(prev, -1);
-        int maxIndex = 0;
-        for (int i = 1; i < n; i++) {
+
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (
-                    check(words[i], words[j]) &&
-                    dp[j] + 1 > dp[i] &&
-                    groups[i] != groups[j]
-                ) {
-                    dp[i] = dp[j] + 1;
-                    prev[i] = j;
+                if (groups[i] != groups[j] &&
+                    words[i].length() == words[j].length() &&
+                    hammingDistance(words[i], words[j]) == 1) {
+                    
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        prev[i] = j;
+                    }
                 }
             }
-            if (dp[i] > dp[maxIndex]) {
-                maxIndex = i;
+        }
+
+        int maxIdx = 0;
+        for (int i = 1; i < n; i++) {
+            if (dp[i] > dp[maxIdx]) {
+                maxIdx = i;
             }
         }
-        List<String> ans = new ArrayList<>();
-        for (int i = maxIndex; i >= 0; i = prev[i]) {
-            ans.add(words[i]);
+
+        LinkedList<String> result = new LinkedList<>();
+        while (maxIdx != -1) {
+            result.addFirst(words[maxIdx]);
+            maxIdx = prev[maxIdx];
         }
-        Collections.reverse(ans);
-        return ans;
+
+        return result;
     }
 
-    private boolean check(String s1, String s2) {
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-        int diff = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                if (++diff > 1) {
-                    return false;
-                }
+    private int hammingDistance(String a, String b) {
+        int dist = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                dist++;
             }
         }
-        return diff == 1;
+        return dist;
     }
 }
