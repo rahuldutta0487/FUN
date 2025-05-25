cb43ce34aded1581ea2f2ab0cbc3104c7fbee516
Rahul Dutta <125856707+rahuldutta0487@users.noreply.github.com>
2025-05-25 14:00:03 +0530

    Time: 4 ms (97.22%), Space: 61.6 MB (73.61%) - LeetHub

---
+++ 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int bestRotation(int[] nums) {
+        final int size = nums.length;
+        int[] rsc = new int[size];
+        for(int i = 0; i < size - 1; i++) {
+            int value = nums[i];
+            int downPos = (i + 1 + size - value) % size;
+            rsc[downPos]--;
+        }
+        int value = nums[size-1];
+        if( value != 0 ) rsc[size - value]--;
+        int bestk = 0;
+        int bestscore = rsc[0];
+        int score = rsc[0];
+        for(int i = 1; i < nums.length; i++) {
+            score += rsc[i] + 1;
+            if( score > bestscore ) {
+                bestk = i;
+                bestscore = score;
+            }
+        }
+        return bestk;
+    }
+}
