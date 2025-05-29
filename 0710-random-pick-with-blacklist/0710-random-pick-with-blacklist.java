489540d092ce6843d9b44c7c2dbc9ea71e874e50
Rahul Dutta <125856707+rahuldutta0487@users.noreply.github.com>
2025-05-29 13:05:43 +0530

    Time: 46 ms (38%), Space: 57.5 MB (8.5%) - LeetHub

---
+++ 0710-random-pick-with-blacklist/0710-random-pick-with-blacklist.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    Map<Integer, Integer> m;
+    Random r;
+    int wlen;
+
+    public Solution(int n, int[] b) {
+        m = new HashMap<>();
+        r = new Random();
+        wlen = n - b.length;
+        Set<Integer> w = new HashSet<>();
+        for (int i = wlen; i < n; i++) w.add(i);
+        for (int x : b) w.remove(x);
+        Iterator<Integer> wi = w.iterator();
+        for (int x : b)
+            if (x < wlen)
+                m.put(x, wi.next());
+    }
+
+    public int pick() {
+        int k = r.nextInt(wlen);
+        return m.getOrDefault(k, k);
+    }
+}
