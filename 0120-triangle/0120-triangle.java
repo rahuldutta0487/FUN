09703535cd031ad51018120cfae7f9d1266383e2
Rahul Dutta <125856707+rahuldutta0487@users.noreply.github.com>
2025-09-25 13:36:10 +0530

    Time: 2 ms (86.07%), Space: 43.8 MB (99.99%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        
+        // Initialize the dp array with the last row of the triangle
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+        
+        // Build the dp array from bottom to top
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        
+        return dp[0];
+    }
+}
