65de02b98e5bba15e9ded19c0dca3b48f5a0d12b
Rahul Dutta <125856707+rahuldutta0487@users.noreply.github.com>
2025-11-10 10:59:04 +0530

    Time: 87 ms (48.59%), Space: 139.4 MB (5.63%) - LeetHub

---
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            Set<Integer> set = new HashSet<>();
+            while (!stack.isEmpty() && stack.peek() > nums[i]) {
+                set.add(stack.pop());
+            }
+            ans += set.size();
+            stack.push(nums[i]);
+        }
+        Set<Integer> set = new HashSet<>();
+        while (!stack.isEmpty()) {
+            set.add(stack.pop());
+        }
+        set.remove(0);
+        return ans + set.size();
+    }
+}
