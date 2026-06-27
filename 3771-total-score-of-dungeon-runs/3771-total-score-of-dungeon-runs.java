8a6f29de22d3e597f54668e67524d18647bb9b1c
Rahul Dutta <125856707+rahuldutta0487@users.noreply.github.com>
2026-06-27 10:49:52 -0700

    Time: 19 ms (100%), Space: 170.3 MB (90%) - LeetHub

--- 3771-total-score-of-dungeon-runs/3771-total-score-of-dungeon-runs.java
+++ 3771-total-score-of-dungeon-runs/3771-total-score-of-dungeon-runs.java
@@ -1,78 +1,34 @@
-import java.util.Arrays;
-import java.util.Map;
-import java.util.HashMap;
-
 class Solution {
-    
-    private static class FenwickTree {
-        long[] ft;
-        int N;
-
-        public FenwickTree(int size) {
-            this.N = size;
-            this.ft = new long[size + 1];
-        }
-
-        public void update(int idx, long delta) {
-            for (; idx <= N; idx += idx & -idx) {
-                ft[idx] += delta;
-            }
-        }
-
-        public long query(int idx) {
-            long sum = 0;
-            for (; idx > 0; idx -= idx & -idx) {
-                sum += ft[idx];
-            }
-            return sum;
-        }
-    }
-
     public long totalScore(int hp, int[] damage, int[] requirement) {
         int n = damage.length;
-        
-        long[] cumDmg = new long[n + 1];
-        for (int i = 0; i < n; i++) {
-            cumDmg[i + 1] = cumDmg[i] + damage[i];
-        }
+        long soln = 0;
+        int[] cumulative = new int[n + 1];
+        //Set cumulative
+        for (int i = n - 1; i >= 0; i--)
+            cumulative[i] = cumulative[i + 1] + damage[i];
 
-        long[] vals = new long[2 * n + 1];
-        System.arraycopy(cumDmg, 0, vals, 0, n + 1);
-        for (int k = 0; k < n; k++) {
-            vals[n + 1 + k] = cumDmg[k + 1] + requirement[k] - hp;
-        }
-        
-        long[] uniqueSortedVals = Arrays.stream(vals).distinct().sorted().toArray();
-        
-        Map<Long, Integer> valToIdx = new HashMap<>();
-        for (int i = 0; i < uniqueSortedVals.length; i++) {
-            valToIdx.put(uniqueSortedVals[i], i + 1);
-        }
-        
-        int compressedSize = uniqueSortedVals.length;
-        FenwickTree bit = new FenwickTree(compressedSize);
-        long ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int adjHP = hp - requirement[i] + cumulative[i + 1];
+            if (adjHP < cumulative[i])
+                continue;
+            else if (adjHP > cumulative[0])
+                soln += i + 1;
+            else
+                soln += i - binSearch(cumulative, adjHP, i) + 1;
 
-        for (int k = 0; k < n; k++) {
-            int cIdx = valToIdx.get(cumDmg[k]);
-            bit.update(cIdx, 1);
-            
-            long T = cumDmg[k + 1] + requirement[k] - hp;
-
-            int searchIdx = Arrays.binarySearch(uniqueSortedVals, T);
-            if (searchIdx < 0) {
-                searchIdx = -searchIdx - 1;
-            }
-            
-            long countLT = bit.query(searchIdx); 
-            
-            long totalElements = bit.query(compressedSize);
-            
-            long countGE = totalElements - countLT;
-            
-            ans += countGE;
         }
+        return soln;
+    }
 
-        return ans;
+    int binSearch(int[] arr, int hp, int l) {
+        int h = 0;
+        while (h < l) {
+            int mid = (h + l) / 2;
+            if (arr[mid] > hp)
+                h = mid + 1;
+            else
+                l = mid;
+        }
+        return h;
     }
 }
